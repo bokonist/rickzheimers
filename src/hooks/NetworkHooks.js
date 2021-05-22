7ba934fe8f81466c1104f075d7386286889041e3
bokonist <jayrm1995@gmail.com>
2021-05-22 20:31:08 +0530

    simplified custom hook

--- src/hooks/NetworkHooks.js
+++ src/hooks/NetworkHooks.js
@@ -1,30 +1,15 @@
 import { useEffect, useState } from "react";
 
-function cleanURL(url) {
-  let indexStart = 0,
-    indexEnd = url.length - 1;
-  for (let i = url.length - 1; i > 0; i--) {
-    if (url[i] === "/") {
-      indexEnd--;
-    }
-  }
-  for (let j = 0; j < url.length; j++) {
-    if (url[j] === "/") {
-      indexStart++;
-    }
-  }
-  return url.slice(indexStart, indexEnd);
-}
-
-const useHttp = (baseurl, endpoint, parameterMap = {}, dependencies) => {
+const useHttp = (url, dependencies) => {
   const [isLoading, setIsLoading] = useState(false);
   const [fetchedData, setFetchedData] = useState(null);
 
-  let url = "http://" + cleanURL(baseurl) + "/" + cleanURL(endpoint) + "?";
+  /*  parameterMap = new Map(parameterMap);
   for (const [key, value] of parameterMap) {
     url += key + "=" + value + "&";
-  }
+  }*/
   useEffect(() => {
+    console.log("data being fetched");
     setIsLoading(true);
     fetch(url)
       .then((response) => {
